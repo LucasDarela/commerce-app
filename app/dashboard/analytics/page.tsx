22f6470faefea499f3f27286eec6cf88d133ddf6
Darela <85245283+LucasDarela@users.noreply.github.com>
2025-03-27 18:38:34 -0300

    removing shets

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -1,32 +1,10 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
-import { useRouter } from "next/navigation";
-import { supabase } from "@/lib/supabase";
-
-
-
 
 export default function Analytics() {
   
-
   return (
 <div className="p-8">
-
-      {/* 🔹 Campo de Pesquisa */}
       <div className="mb-4 flex flex-col-2 gap-6">
-        <Input
-          type="text"
-          placeholder="Pesquise por CPF ou Telefone..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="w-full p-2 border rounded-md"
-        />
-                <Button onClick={() => router.push("/dashboard")} className="w-full sm:w-auto">
-          Analytics
-        </Button>
+        <h3>Analitics</h3>
       </div>
     </div>
   );
