651ed65396bebeb6635da68208e7964b1262f7c3
Darela <lucasdarela@live.com>
2025-09-01 14:29:11 -0300

    fixing bugs

---
+++ app/dashboard/(admin)/ClientChart.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+// se seu export for default, troque a linha acima para:
+// import ChartAreaInteractive from "@/components/chart-area-interactive";
+
+export default function ClientChart() {
+  return <ChartAreaInteractive />;
+}
